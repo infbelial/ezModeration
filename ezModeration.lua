7212589be03f173af8347f9ab62411ba9715fcca
infbelial <makethemrepentasmodai@gmail.com>
2026-06-05 12:54:23 +0300

    Предупреждение о старом аддоне

--- ezModeration.lua
+++ ezModeration.lua
@@ -24,6 +24,21 @@ addon:RegisterEvent("PLAYER_LOGIN")
 addon:RegisterEvent("CHAT_MSG_ADDON")
 addon:RegisterEvent("ADDON_LOADED")
 
+local function ShowWarning()
+    StaticPopupDialogs["EZMODERATION_ADDON_WARNING"] = {
+	    text = EZMOD_TEXT_ADDON_WARNING,
+	    button1 = OKAY,
+	    button2 = nil,
+	    OnAccept = function() end,
+	    hideOnEscape = 1,
+	    timeout = 0,
+	    exclusive = 1,
+	    whileDead = 1,
+    }
+    StaticPopup_Show("EZMODERATION_ADDON_WARNING")
+end
+
+
 addon:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_LOGIN" then
         ezMod:SendMessage(string.format("INIT:%d;", ezMod.rules.version))
@@ -32,6 +47,8 @@ addon:SetScript("OnEvent", function(self, event, ...)
         if name == "ezModeration" then
             ezMod:Init()
             EzModChatRulesPanel_Update()
+        elseif name == "SMARTModeration2" then
+            ShowWarning()
         end
     elseif event == "CHAT_MSG_ADDON" then
         local prefix, message, channel, sender = ...
