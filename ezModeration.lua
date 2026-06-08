3b103139f3c714fd8f5702061ff7d63e12a2efef
infbelial <makethemrepentasmodai@gmail.com>
2026-06-08 13:16:09 +0300

    Упрощение команды вызова

--- ezModeration.lua
+++ ezModeration.lua
@@ -2,20 +2,8 @@ SLASH_EZMOD1 = "/ezmod"
 SLASH_EZMOD2 = "/ezm"
 
 SlashCmdList["EZMOD"] = function(msg)
-    if msg == "show" or msg == "" then
-        ShowUIPanel(EzModModerationFrame)
-    elseif msg == "hide" then
-        HideUIPanel(EzModModerationFrame)
-    elseif msg == "rules" then
-        ShowUIPanel(EzModModerationFrame)
-        EzModModerationFrame_SetTab(EzModModerationFrame, 2)
-    else
-        if msg:sub(1, 3) == "mod" then
-            EzModModerationPanel_NewSearch(msg:sub(5))
-        else
-            print(EZMOD_TEXT_USAGE)
-        end
-    end
+    ShowUIPanel(EzModModerationFrame)
+    EzModModerationPanel_NewSearch(msg)
 end
 
 local addon = CreateFrame("Frame", nil, UIParent)
