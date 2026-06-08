97132d08fd0e0f83cfc98078e5f20c87563cafc7
infbelial <makethemrepentasmodai@gmail.com>
2026-06-08 14:55:05 +0300

    Обрабатываем alt+клик как в старом аддоне

--- ezModeration.lua
+++ ezModeration.lua
@@ -26,6 +26,29 @@ local function ShowWarning()
     StaticPopup_Show("EZMODERATION_ADDON_WARNING")
 end
 
+local original = nil
+
+-- logic copied from previous adddon
+local function HandleNameClick(link, text, button)
+	local linkType = string.sub(link, 1, string.find(link, ':') - 1)
+	
+	if IsAltKeyDown() then
+		if linkType == 'player' then
+			local name = strsplit(':', string.sub(link, 8))
+			if name and name:len() > 0 then
+				local begin = name:find('%s[^%s]+$')
+				if begin then
+					name = name:sub(begin + 1)
+				end
+                ShowUIPanel(EzModModerationFrame)
+                EzModModerationPanel_NewSearch(name)
+			end
+		end
+	else
+		original(link, text, button)
+	end
+end
+
 
 addon:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_LOGIN" then
@@ -35,6 +58,8 @@ addon:SetScript("OnEvent", function(self, event, ...)
         if name == "ezModeration" then
             ezMod:Init()
             EzModChatRulesPanel_Update()
+            original = SetItemRef
+            SetItemRef = HandleNameClick
         elseif name == "SMARTModeration2" then
             ShowWarning()
         end
